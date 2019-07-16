36fe24eb4ecd95a7a9ef034132f41652f3cd2681
LLeiSong <pinot.sung@gmail.com>
2019-07-15 22:46:02 -0400

    Add scripts to register raster foundry

---
+++ planet/rasterfoundry_register_each.py
@@ -0,0 +1,21 @@
+import yaml
+import argparse
+from rf_client import *
+
+# read config
+with open("cfg/config.yaml", 'r') as yaml_file:
+    config = yaml.load(yaml_file)
+
+# rfclient init
+rfclient = RFClient(config)
+
+def main(scene_id, url):
+    tms_url = rfclient.create_tms_uri(scene_id, url)
+    print(tms_url)
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = 'Generate new TMS links for each image')
+    parser.add_argument('--scene_id', help='The scene id of the image')
+    parser.add_argument('--url', help='The url of the image')
+    args = parser.parse_args()
+    main(args.scene_id, args.url)
