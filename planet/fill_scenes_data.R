e7ab15435f542f7f0896364dd216a16eed5f8955
LLeiSong <pinot.sung@gmail.com>
2019-08-26 11:52:21 -0400

    Modify scripts for multi-scenes project

--- planet/fill_scenes_data.R
+++ planet/fill_scenes_data.R
@@ -16,6 +16,13 @@ planet_catalog_path <- file.path(params$imagery$catalog_path,
                                  params$imagery$catalog_filename)
 planet_catalog <- read.csv(planet_catalog_path)
 
+grids_tile_path <- file.path(params$imagery$catalog_path,
+                                 params$imagery$grids_tile_filename)
+grids_tile <- read.csv(grids_tile_path)
+
+planet_catalog_tile <- merge(planet_catalog, grids_tile,
+                             by.x = "cell_id", by.y = "id")
+
 ## Register only once for the same image
 imgs <- unique(planet_catalog$url) %>% 
                   data.table(url = .)
@@ -27,25 +34,67 @@ imgs_tb <- merge(imgs, planet_catalog[, c("scene_id", "url")],
 coninfo <- mapper_connect(host = params$database$db_host,
                           user = "sandbox")
 scenes_data <- tbl(coninfo$con, "scenes_data") %>% 
-  filter(scene_id %in% !!imgs_tb$scene_id) %>% 
-  dplyr::select(scene_id) %>% 
+  filter(scene_id %in% !!imgs_tb$scene_id) %>%
+  dplyr::select(cell_id, scene_id) %>% 
   collect() %>%
+  filter(stringr::str_detect(scene_id, 'tile')) %>%
   distinct()
 imgs_tb <- imgs_tb %>%
   filter(!scene_id %in% scenes_data$scene_id)
 
+# Register scenes
 num_cores <- min(nrow(imgs_tb), detectCores() - 1)
-get_tms_url <- mclapply(1:nrow(imgs_tb), function(i){
+register_scene <- mclapply(1:nrow(imgs_tb), function(i){
   scene_id <- imgs_tb[i, ]$scene_id
   url <- imgs_tb[i, ]$url
-  cmd_text <- sprintf(paste0("python3 rasterfoundry_register_each.py ",
+  cmd_text <- sprintf(paste0("python3 rasterfoundry_register_scene.py ",
                      "--scene_id %s ",
                      "--url %s"),
                      scene_id, url)
-  tms_url <- system(cmd_text, intern = TRUE)
-  data.table(scene_id = scene_id, tms_url = tms_url)
+  scene_rf_id <- system(cmd_text, intern = TRUE)
+  data.table(scene_id = scene_id, scene_rf_id = scene_rf_id)
 }, mc.cores = num_cores)
-planet_catalog_unique <- do.call(rbind, get_tms_url)
+planet_scene_rf_unique <- do.call(rbind, register_scene)
+
+# Build project
+build_project <- mclapply(1:nrow(imgs_tb), function(i) {
+  scene_id_this <- imgs_tb[i, ]$scene_id
+  
+  # Get the neighborhood
+  tile_ng_this <- planet_catalog_tile %>%
+    filter(scene_id == scene_id_this) %>%
+    select(tile, tile_ng) %>%
+    distinct()
+  tile_ng_this <- as.integer(strsplit(
+    as.character(unique(tile_ng_this$tile_ng)), ",")[[1]])
+  
+  season_this <- planet_catalog_tile %>%
+    filter(scene_id == scene_id_this) %>%
+    select(season) %>%
+    distinct()
+  
+  scene_ids <- planet_catalog_tile %>%
+    filter((tile %in% tile_ng_this) & 
+             (season == season_this$season)) %>%
+    select(scene_id) %>%
+    distinct()
+  
+  scene_rf_ids <- planet_scene_rf_unique %>%
+    filter(scene_id %in% scene_ids$scene_id)
+  
+  scene_rf_ids <- paste(scene_rf_ids$scene_rf_id, collapse = ",")
+  cmd_text <- sprintf(paste0("python3 rasterfoundry_create_project.py ",
+                             "--scene_id %s ",
+                             "--scene_ids %s"),
+                      as.character(scene_id_this), 
+                      scene_rf_ids)
+  tms_url <- system(cmd_text, intern = TRUE)
+  data.table(scene_id = as.character(scene_id_this), 
+             tms_url = tms_url)
+}, mc.cores = num_cores)
+planet_catalog_unique <- do.call(rbind, build_project)
+save(planet_catalog_unique, file = "/Users/pinot/downloads/planet_catalog_unique.rda")
+
 planet_catalog <- merge(planet_catalog, planet_catalog_unique, by = "scene_id")
 
 # Updates the database
@@ -77,3 +126,4 @@ insert_db <- lapply(1:nrow(planet_catalog), function(i) {
                   dbExecute(coninfo$con, sql)
                   # db_commit(coninfo$con)
 })
+# db_commit(coninfo$con)
