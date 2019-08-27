f18de0e982e4c68e0fdacf2c63c5fadb03b46ff5
Ubuntu <ubuntu@ip-172-31-41-140.ec2.internal>
2019-08-27 02:10:45 +0000

    Small fix

--- scripts/run_composition_aoi.sh
+++ scripts/run_composition_aoi.sh
@@ -8,5 +8,4 @@ read aoi_id
 
 conda activate composite
 python $composite_exe --aoi=$aoi_id --threads_number=$threads_num --config_filename=$config_filenm
-mail -s "Composition Finished!" sye@clarku.edu <<< 'The composition task for $aoi_id finished!'
-
+mail -s "Composition Finished!" sye@clarku.edu <<< 'The composition task for finished: aoi'$aoi_id
