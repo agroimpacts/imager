bcca1c1a253e5a17475d78a8fccb82e5f204eec4
LLeiSong <pinot.sung@gmail.com>
2019-09-03 11:45:06 -0400

    Move some steps to labeller

--- planet/rasterfoundry_clean.py
+++ planet/rasterfoundry_clean.py
@@ -1,5 +1,5 @@
 import yaml
-import argparse
+import requests
 from rf_client import *
 
 # read config
@@ -10,9 +10,42 @@ with open("cfg/config.yaml", 'r') as yaml_file:
 rfclient = RFClient(config)
 
 
+def list_owned_scenes(user_id, jwt):
+    def make_request(headers, page, page_size=1000):
+        return requests.get(
+            'https://{host}/api/scenes'.format(host=rf_host),
+            headers=headers,
+            params={
+                'pageSize': page_size,
+                'page': page,
+                'owner': user_id
+            }
+        )
+    headers = {'Authorization': jwt}
+    rf_host = 'app.rasterfoundry.com'
+    page = 0
+    resp = make_request(headers, page)
+    resp.raise_for_status()
+    js = resp.json()
+    scenes = js['results']
+    has_next = js['hasNext']
+
+    while has_next:
+        page += 1
+        resp = make_request(headers, page)
+        resp.raise_for_status()
+        js = resp.json()
+        scenes.extend(js['results'])
+        has_next = js['hasNext']
+
+    return scenes
+
+
 def main():
     rfclient.delete_all_projects()
-    rfclient.delete_all_scenes()
+    scenes = list_owned_scenes(user_id=rfclient.owner, jwt=rfclient.api.api_token)
+    for scene in scenes:
+        rfclient.api.client.Imagery.delete_scenes_sceneID(sceneID=scene['id']).result()
 
 
 if __name__ == "__main__":
